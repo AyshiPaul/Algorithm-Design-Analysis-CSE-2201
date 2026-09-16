4018ac8a6a886cbeaafc5dbde20312aa33176d04
Ayshi Paul Asha <ayshipaul00@gmail.com>
2026-09-16 12:58:00 +0600

    Add files via upload

---
+++ Fibonacci Series/Febonacci Series using recursion.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int fibo( int n)
+{
+    if (n == 0)
+        return 0;
+    else if ( n == 1)
+        return 1;
+    else
+        return fibo(n-1) + fibo (n-2);
+}
+
+int main()
+{
+    int n ;
+
+    printf("Enter the value of N : ");
+    scanf("%d", &n);
+
+    printf("Fibonacci Number : %d ", fibo(n));
+
+    return 0;
+}
+
---
+++ Fibonacci Series/Fibonacci series using loop.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main()
+{
+    int n , i ;
+    int a = 0, b = 1, c;
+
+    printf("Enter the value of N : ");
+    scanf("%d", &n);
+
+    printf("Fibonacci Series : ");
+
+    for( i = 0; i<= n; i++)
+    {
+        printf("%d ", a);
+        c = a+b;
+        a = b;
+        b = c;
+    }
+
+    return 0;
+}
